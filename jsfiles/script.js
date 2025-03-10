4778e03ba5f5edf58bd0e1d239d78acac6d65abb
letsinnovate-code <ap1106561@gmail.com>
2025-03-10 20:33:58 +0530

    new changes

--- jsfiles/script.js
+++ jsfiles/script.js
@@ -1,5 +1,7 @@
 const todoBtn = document.getElementById("todo-btn")
 const todoContainer = document.getElementById("todo-container")
+const boardBtn = document.getElementById("board-btn")
+const mainContainer = document.getElementById("main-container")
 
 todoBtn.addEventListener("click",()=>{
     const task = prompt('Please Add Your Task')
@@ -8,12 +10,21 @@ todoBtn.addEventListener("click",()=>{
     const para = document.createElement('p')
     para.classList.add('task')
     para.innerText = task;
-
     todoItem.appendChild(para);
     todoContainer.appendChild(todoItem)
-    console.log(todoContainer);
+    console.log(todoContainer);    
     
-    
-    
-    
+})
+
+boardBtn.addEventListener('click',()=>{
+    const newBoardName = prompt("add name of new board")
+    const newBoard = document.createElement('div')
+    newBoard.classList.add("containers")
+    const boardName = document.createElement('h1')
+    boardName.innerText = newBoardName;
+    newBoard.appendChild(boardName)
+    mainContainer.appendChild(newBoard)
+    console.log(mainContainer)
+
+
 })
