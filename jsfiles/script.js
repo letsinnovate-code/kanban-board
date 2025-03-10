5c8f483860bbeafffb1d7d3b62991b20acfab528
letsinnovate-code <ap1106561@gmail.com>
2025-03-10 21:08:11 +0530

    more changes

--- jsfiles/script.js
+++ jsfiles/script.js
@@ -2,6 +2,7 @@ const todoBtn = document.getElementById("todo-btn")
 const todoContainer = document.getElementById("todo-container")
 const boardBtn = document.getElementById("board-btn")
 const mainContainer = document.getElementById("main-container")
+const removeTask = document.getElementById("remove-task")
 
 todoBtn.addEventListener("click",()=>{
     const task = prompt('Please Add Your Task')
@@ -12,7 +13,42 @@ todoBtn.addEventListener("click",()=>{
     para.innerText = task;
     todoItem.appendChild(para);
     todoContainer.appendChild(todoItem)
-    console.log(todoContainer);    
+
+
+
+    // adding delete button 
+
+
+
+    const removeTask = document.createElement("button")
+    // console.log(removeTask);
+    removeTask.innerText="Delete"
+    removeTask.setAttribute("id",removeTask)
+    todoItem.append(removeTask)
+    removeTask.addEventListener('click',()=>{
+        todoItem.remove()
+    })
+    
+    // console.log(todoContainer);    
+
+    // adding an edit button using js
+    
+    const editTask = document.createElement("button")
+    editTask.innerText= "Edit"
+    editTask.setAttribute("id",editTask)
+    todoItem.append(editTask)
+    editTask.addEventListener('click',()=>{
+        para.contentEditable=true
+        const doneBtn = document.createElement("button")
+        doneBtn.setAttribute('id',doneBtn)
+        doneBtn.innerText = 'Done'
+        todoItem.append(doneBtn)
+        doneBtn.addEventListener('click',()=>{
+            para.contentEditable=false
+            doneBtn.remove()
+        })
+        
+    })
     
 })
 
@@ -24,7 +60,18 @@ boardBtn.addEventListener('click',()=>{
     boardName.innerText = newBoardName;
     newBoard.appendChild(boardName)
     mainContainer.appendChild(newBoard)
-    console.log(mainContainer)
+    // console.log(mainContainer)
+
+    const removeTask = document.createElement("button")
+    // console.log(removeTask);
+    removeTask.innerText="Delete"
+    removeTask.setAttribute("id",removeTask)
+    newBoard.append(removeTask)
+    removeTask.addEventListener('click',()=>{
+        newBoard.remove()
+    })
 
 
 })
+
+
